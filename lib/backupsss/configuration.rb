f8315ceeb8f82948a75c8b7192d3607e4646002d
RWalker8 <reppard.walker@manheim.com>
2016-02-08 16:40:32 -0500

    feat(configuration.rb): add region with default

--- lib/backupsss/configuration.rb
+++ lib/backupsss/configuration.rb
@@ -7,10 +7,15 @@ module Backupsss
         :s3_bucket,
         :s3_bucket_key,
         :backup_src_dir,
-        :backup_frequency
+        :backup_frequency,
+        :aws_region
       ]
     end
 
     attr_accessor(*keys)
+
+    def initialize
+      @aws_region = 'us-east-1'
+    end
   end
 end
