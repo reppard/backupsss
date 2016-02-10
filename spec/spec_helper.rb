22de9d393d695bc3299d36920423a0a906af8ffa
RWalker8 <reppard.walker@manheim.com>
2016-02-10 15:45:53 -0500

    feat(backupsss.gemspec,spec_helper.rb): simplecov-console

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::Console,
+]
 SimpleCov.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
