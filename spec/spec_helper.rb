482ec0e609822d2fc55681142aea52d829fd3594
RWalker8 <reppard.walker@manheim.com>
2016-02-10 15:52:11 -0500

    chore(spec_helper.rb): sometimes ignore stdout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,15 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 SimpleCov.start
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+RSpec.configure do |config|
+  real_stdout = $stdout
+
+  config.before(:example, ignore_stdout: true) do
+    $stdout = File.open(File::NULL, 'w')
+  end
+
+  config.after(:example, ignore_stdout: true) do
+    $stdout = real_stdout
+  end
+end
