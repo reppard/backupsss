d5d7340e89a0332814eb3789fa0066663b26dbd3
RWalker8 <reppard.walker@manheim.com>
2016-02-11 08:13:32 -0500

    refactor(local_janitor.rb): refactor methods

--- lib/backupsss/local_janitor.rb
+++ lib/backupsss/local_janitor.rb
@@ -1,7 +1,7 @@
 module Backupsss
-  # Backupsss::Janitor
+  # A class for cleaning up backup artifacts
   class LocalJanitor
-    attr_accessor :dir
+    attr_reader :dir
 
     def initialize(dir)
       @dir = dir
@@ -9,23 +9,27 @@ module Backupsss
 
     def ls_garbage
       garbage = Dir.entries(dir).reject { |f| (f == '..' || f == '.') }
-      puts garbage_message(garbage)
+      puts garbage_message(filter_garbage)
       garbage
     end
 
     def rm_garbage(file_array)
-      file_array.each do |f|
-        begin
-          FileUtils.rm(File.join(dir, f))
-          puts "Cleaning up #{f}"
-        rescue SystemCallError => e
-          puts "Could not clean up #{f}: #{e.message}"
-        end
-      end
-      puts 'Finished cleaning up.'
+      file_array.each { |f| burn_item(f) }
+      display_finished
     end
 
     private
+
+    def burn_item(item)
+      FileUtils.rm(File.join(dir, item))
+      display_cleanup(item)
+    rescue SystemCallError => e
+      display_error(e, item)
+    end
+
+    def filter_garbage
+      Dir.entries(dir).reject { |f| (f == '..' || f == '.') }
+    end
 
     def garbage_message(garbage)
       if garbage.empty?
@@ -33,6 +37,18 @@ module Backupsss
       else
         ['Found garbage...', garbage.sort].flatten.join("\n")
       end
+    end
+
+    def display_finished
+      puts 'Finished cleaning up.'
+    end
+
+    def display_cleanup(item)
+      puts "Cleaning up #{item}"
+    end
+
+    def display_error(e, item)
+      puts "Could not clean up #{item}: #{e.message}"
     end
   end
 end
