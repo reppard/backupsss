a02997c2d41526719f3014213959e98fddbf5de4
RWalker8 <reppard.walker@manheim.com>
2016-02-15 10:39:51 -0500

    style(tar_spec.rb): use is_expected syntax

--- spec/backupsss/tar_spec.rb
+++ spec/backupsss/tar_spec.rb
@@ -2,183 +2,89 @@ require 'spec_helper'
 require 'backupsss/tar'
 
 describe Backupsss::Tar do
-  describe '#valid_src?' do
-    let(:src) do
-      'spec/fixtures/backup_src'
-    end
+  let(:src)  { 'spec/fixtures/backup_src' }
+  let(:dest) { 'spec/fixtures/backups/backup.tar' }
+  before     { allow(File).to receive(:exist?) { true } }
 
-    subject do
-      Backupsss::Tar.new(src, '')
-    end
+  describe '#valid_src?' do
+    subject { -> { Backupsss::Tar.new(src, '').valid_src? } }
 
     context 'when src does not exist on the file system' do
-      before do
-        allow(File).to receive(:exist?) { false }
-      end
+      before { allow(File).to receive(:exist?) { false } }
 
-      it 'will raise Errno::ENOENT' do
-        expect { subject.valid_src? }.to raise_error(Errno::ENOENT)
-      end
-
-      it 'will raise with the value of src in the message' do
-        expect { subject.valid_src? }.to raise_error(/#{src}$/)
-      end
+      it { is_expected.to raise_error(Errno::ENOENT) }
+      it { is_expected.to raise_error(/#{src}$/) }
     end
 
     context 'when src is not readable' do
-      before do
-        allow(File).to receive(:exist?)    { true }
-        allow(File).to receive(:readable?) { false }
-      end
+      before { allow(File).to receive(:readable?) { false } }
 
-      it 'will raise Errno::EPERM' do
-        expect { subject.valid_src? }.to raise_error(Errno::EPERM)
-      end
-
-      it 'will raise with the value of src in the message' do
-        expect { subject.valid_src? }.to raise_error(/#{src}$/)
-      end
+      it { is_expected.to raise_error(Errno::EPERM) }
+      it { is_expected.to raise_error(/#{src}$/) }
     end
 
     context 'when src exists and is readable' do
-      before do
-        allow(File).to receive(:exist?)    { true }
-        allow(File).to receive(:readable?) { true }
-      end
+      before  { allow(File).to receive(:readable?) { true } }
+      subject { Backupsss::Tar.new(src, '').valid_src? }
 
-      it 'will return true' do
-        expect(subject.valid_src?).to be true
-      end
+      it { is_expected.to be true }
     end
   end
 
   describe '#valid_dest?' do
-    let(:dest) do
-      'spec/fixtures/backups/backup.tar'
-    end
-
-    subject do
-      Backupsss::Tar.new('', dest)
-    end
+    subject { -> { Backupsss::Tar.new('', dest).valid_dest? } }
 
     context 'when dest dir does not exist on the file system' do
-      before do
-        allow(File).to receive(:exist?) { false }
-      end
+      before { allow(File).to receive(:exist?) { false } }
 
-      it 'will raise Errno::ENOENT' do
-        expect { subject.valid_dest? }.to raise_error(Errno::ENOENT)
-      end
-
-      it 'will raise with the dirname of dest in the message' do
-        err_regex = /#{File.dirname(dest)}$/
-        expect { subject.valid_dest? }.to raise_error(err_regex)
-      end
+      it { is_expected.to raise_error(Errno::ENOENT) }
+      it { is_expected.to raise_error(/#{File.dirname(dest)}$/) }
     end
 
     context 'when dest dir is not writable' do
-      before do
-        allow(File).to receive(:exist?)    { true }
-        allow(File).to receive(:writable?) { false }
-      end
+      before { allow(File).to receive(:readable?) { false } }
 
-      it 'will raise Errno::EPERM' do
-        expect { subject.valid_dest? }.to raise_error(Errno::EPERM)
-      end
-
-      it 'will raise with the dirname of dest in the message' do
-        err_regex = /#{File.dirname(dest)}$/
-        expect { subject.valid_dest? }.to raise_error(err_regex)
-      end
+      it { is_expected.to raise_error(Errno::EPERM) }
+      it { is_expected.to raise_error(/#{File.dirname(dest)}$/) }
     end
 
     context 'when dest dir exists and is writable' do
-      before do
-        allow(File).to receive(:exist?)    { true }
-        allow(File).to receive(:writable?) { true }
-      end
+      before  { allow(File).to receive(:writable?) { true } }
+      subject { Backupsss::Tar.new('', dest).valid_dest? }
 
-      it 'will return true' do
-        expect(subject.valid_dest?).to be true
-      end
+      it { is_expected.to be true }
     end
   end
 
   describe '#make' do
-    context 'when src and dest dir exist with correct permissions' do
-      before do
-        FileUtils.mkdir_p(src)
-        FileUtils.mkdir_p(File.dirname(dest))
-      end
-
-      after do
-        FileUtils.rm_rf(src)
-        FileUtils.rm_rf(File.dirname(dest))
-      end
-
-      let(:src) do
-        'spec/fixtures/backup_src'
-      end
-
-      let(:dest) do
-        'spec/fixtures/backup/backup.tar'
-      end
-
-      subject do
-        Backupsss::Tar.new(src, dest)
-      end
-
-      it 'will return a file' do
-        expect(subject.make).to be_a(File)
-      end
-    end
+    subject { -> { Backupsss::Tar.new('some/src', 'some/dest.tar').make } }
 
     context 'when src is not readable' do
-      before do
-        allow(File).to receive(:readable?) { false }
-        allow(File).to receive(:writable?) { true }
-        allow(File).to receive(:exist?)    { true }
-      end
+      before { allow(File).to receive(:readable?) { false } }
 
-      subject do
-        Backupsss::Tar.new('some/src', 'some/dest.tar')
-      end
-
-      it 'will re-raise Errno::EPERM' do
-        expect { subject.make }.to raise_error(Errno::EPERM)
-      end
+      it { is_expected.to raise_error(Errno::EPERM) }
     end
 
     context 'when dest is not writable' do
-      before do
-        allow(File).to receive(:writable?) { false }
-        allow(File).to receive(:readable?) { true }
-        allow(File).to receive(:exist?)    { true }
-      end
+      before { allow(File).to receive(:writable?) { false } }
 
-      subject do
-        Backupsss::Tar.new('some/src', 'some/dest.tar')
-      end
-
-      it 'will re-raise Errno::EPERM' do
-        expect { subject.make }.to raise_error(Errno::EPERM)
-      end
+      it { is_expected.to raise_error(Errno::EPERM) }
     end
 
     context 'when src or dest do not exist' do
-      before do
-        allow(File).to receive(:writable?) { true }
-        allow(File).to receive(:readable?) { true }
-        allow(File).to receive(:exist?)    { false }
-      end
+      before { allow(File).to receive(:exist?) { false } }
 
-      subject do
-        Backupsss::Tar.new('some/src', 'some/dest.tar')
-      end
+      it { is_expected.to raise_error(Errno::ENOENT) }
+    end
 
-      it 'will re-raise Errno::ENOENT' do
-        expect { subject.make }.to raise_error(Errno::ENOENT)
-      end
+    context 'when src and dest dir exist with correct permissions' do
+      before  { FileUtils.mkdir_p(src) }
+      before  { FileUtils.mkdir_p(File.dirname(dest)) }
+      after   { FileUtils.rm_rf(src) }
+      after   { FileUtils.rm_rf(File.dirname(dest)) }
+      subject { Backupsss::Tar.new(src, dest).make }
+
+      it { is_expected.to be_a(File) }
     end
   end
 end
