14a8bd9d9ee56c6546e40efd73d60473bb37a1db
Jonathan Niesen <jonathan.niesen@manheim.com>
2016-02-23 09:17:18 -0500

    chore(spec_helper.rb): use code-climate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 require 'simplecov-console'
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::Console]
 )
