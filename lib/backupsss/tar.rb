98e227ead7d7501dfc9cb30c0c639ae13007320d
Jason Antman <jason@jasonantman.com>
2016-11-22 07:53:26 -0500

    log tar stderr; raise errors if tar exits non-zero, destination file does not exist, or destination file is 0 bytes

--- lib/backupsss/tar.rb
+++ lib/backupsss/tar.rb
@@ -13,7 +13,17 @@ module Backupsss
 
     def make
       if valid_dest? && valid_src?
-        Open3.capture3("#{tar_command} #{dest} #{src}")
+        _, err, status = Open3.capture3("#{tar_command} #{dest} #{src}")
+        STDERR.puts "tar command stderr:\n#{err}" unless err.empty?
+        if status.exitstatus != 0
+          raise "ERROR: #{tar_command} exited #{status.exitstatus}"
+        end
+        unless File.exist?(dest)
+          raise "ERROR: Tar destination file does not exist"
+        end
+        if File.size(dest) == 0
+          raise "ERROR: Tar destionation file is 0 bytes."
+        end
         File.open(dest)
       end
     end
--- spec/backupsss/tar_spec.rb
+++ spec/backupsss/tar_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 require 'backupsss/tar'
 
+def dbl_exitstatus(code)
+  d = double
+  allow(d).to receive(:exitstatus).and_return(code)
+  d
+end
+
 describe Backupsss::Tar do
   let(:src)      { 'spec/fixtures/backup_src' }
   let(:filename) { 'backup.tar' }
@@ -101,11 +107,15 @@ describe Backupsss::Tar do
     context 'when src and dest dir exist with correct permissions' do
       let(:subject) { Backupsss::Tar.new(src, dest) }
       let(:dbl_file) { double(File) }
+      let(:dbl_status) { dbl_exitstatus(0) }
       before(:each) do
         allow(subject).to receive(:valid_dest?).and_return(true)
         allow(subject).to receive(:valid_src?).and_return(true)
         allow(subject).to receive(:tar_command).and_return('tarcmd')
-        allow(Open3).to receive(:capture3)
+        allow(Open3).to receive(:capture3).and_return(['', '', dbl_status])
+        allow(STDERR).to receive(:puts)
+        allow(File).to receive(:exist?).and_return(true)
+        allow(File).to receive(:size).and_return(999)
         allow(File).to receive(:open).and_return(dbl_file)
       end
 
@@ -114,13 +124,103 @@ describe Backupsss::Tar do
         expect(subject).to receive(:valid_src?).once.ordered
         expect(Open3).to receive(:capture3).once.ordered
           .with("tarcmd #{dest} #{src}")
+        expect(STDERR).to_not receive(:puts)
+        expect(File).to receive(:exist?).once.ordered.with(dest)
+        expect(File).to receive(:size).once.ordered.with(dest)
         subject.make
       end
+
       it 'returns the open File object' do
         expect(subject).to receive(:valid_dest?).once.ordered
         expect(subject).to receive(:valid_src?).once.ordered
+        expect(STDERR).to_not receive(:puts)
+        expect(File).to receive(:exist?).once.ordered.with(dest)
+        expect(File).to receive(:size).once.ordered.with(dest)
         expect(File).to receive(:open).once.ordered.with(dest)
         expect(subject.make).to eq(dbl_file)
+      end
+    end
+    context 'when tar exits non-zero' do
+      let(:subject) { Backupsss::Tar.new(src, dest) }
+      let(:dbl_file) { double(File) }
+      let(:dbl_status) { dbl_exitstatus(3) }
+      before(:each) do
+        allow(subject).to receive(:valid_dest?).and_return(true)
+        allow(subject).to receive(:valid_src?).and_return(true)
+        allow(subject).to receive(:tar_command).and_return('tarcmd')
+        allow(Open3).to receive(:capture3)
+          .and_return(['', 'some stderr', dbl_status])
+        allow(STDERR).to receive(:puts)
+        allow(File).to receive(:exist?).and_return(true)
+        allow(File).to receive(:size).and_return(999)
+        allow(File).to receive(:open).and_return(dbl_file)
+      end
+
+      it 'raises an error' do
+        expect(subject).to receive(:valid_dest?).once.ordered
+        expect(subject).to receive(:valid_src?).once.ordered
+        expect(Open3).to receive(:capture3).once.ordered
+          .with("tarcmd #{dest} #{src}")
+        expect(STDERR).to receive(:puts).once.ordered
+          .with("tar command stderr:\nsome stderr")
+        expect(File).to_not receive(:open)
+        expect { subject.make }.to raise_error(
+          RuntimeError, /ERROR: tarcmd exited 3/)
+      end
+    end
+    context 'when destination file does not exist' do
+      let(:subject) { Backupsss::Tar.new(src, dest) }
+      let(:dbl_file) { double(File) }
+      let(:dbl_status) { dbl_exitstatus(0) }
+      before(:each) do
+        allow(subject).to receive(:valid_dest?).and_return(true)
+        allow(subject).to receive(:valid_src?).and_return(true)
+        allow(subject).to receive(:tar_command).and_return('tarcmd')
+        allow(Open3).to receive(:capture3).and_return(['', '', dbl_status])
+        allow(STDERR).to receive(:puts)
+        allow(File).to receive(:exist?).and_return(false)
+        allow(File).to receive(:size).and_return(999)
+        allow(File).to receive(:open).and_return(dbl_file)
+      end
+
+      it 'raises an error' do
+        expect(subject).to receive(:valid_dest?).once.ordered
+        expect(subject).to receive(:valid_src?).once.ordered
+        expect(Open3).to receive(:capture3).once.ordered
+          .with("tarcmd #{dest} #{src}")
+        expect(STDERR).to_not receive(:puts)
+        expect(File).to receive(:exist?).once.with(dest)
+        expect(File).to_not receive(:open)
+        expect { subject.make }.to raise_error(
+          RuntimeError, /ERROR: Tar destination file does not exist/)
+      end
+    end
+    context 'when destination file is 0 bytes' do
+      let(:subject) { Backupsss::Tar.new(src, dest) }
+      let(:dbl_file) { double(File) }
+      let(:dbl_status) { dbl_exitstatus(0) }
+      before(:each) do
+        allow(subject).to receive(:valid_dest?).and_return(true)
+        allow(subject).to receive(:valid_src?).and_return(true)
+        allow(subject).to receive(:tar_command).and_return('tarcmd')
+        allow(Open3).to receive(:capture3).and_return(['', '', dbl_status])
+        allow(STDERR).to receive(:puts)
+        allow(File).to receive(:exist?).and_return(true)
+        allow(File).to receive(:size).and_return(0)
+        allow(File).to receive(:open).and_return(dbl_file)
+      end
+
+      it 'raises an error' do
+        expect(subject).to receive(:valid_dest?).once.ordered
+        expect(subject).to receive(:valid_src?).once.ordered
+        expect(Open3).to receive(:capture3).once.ordered
+          .with("tarcmd #{dest} #{src}")
+        expect(STDERR).to_not receive(:puts)
+        expect(File).to receive(:exist?).once.ordered.with(dest)
+        expect(File).to receive(:size).once.ordered.with(dest)
+        expect(File).to_not receive(:open)
+        expect { subject.make }.to raise_error(
+          RuntimeError, /ERROR: Tar destionation file is 0 bytes\./)
       end
     end
   end
