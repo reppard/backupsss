21c5c10e00a9b7277cb6285b6d8c35e17103138b
reppard <reppardwalker@gmail.com>
2017-01-10 15:00:20 -0500

    unstagemebro

--- lib/backupsss/backup.rb
+++ lib/backupsss/backup.rb
@@ -10,7 +10,36 @@ module Backupsss
     end
 
     def put_file(file)
-      client.put_object(bucket_opts.merge(body: file))
+      # http://docs.aws.amazon.com/AmazonS3/latest/dev/UploadingObjects.html
+      #
+      # > "We encourage Amazon S3 customers to use Multipart Upload for objects
+      # > greater than 100 MB."
+      #
+      # MAX_FILE_SIZE = 104857600 # 100MB
+      # if file.size > MAX_FILE_SIZE
+      #   part_offset = 0
+      #   part_number = 1
+      #   part = file.read(MAX_FILE_SIZE, part_offset)
+      #
+      #   Fork.new {
+      #     s3.upload_part({
+			#       body:        part,
+			#       bucket:      bucket,
+			#       key:         key,
+			#       part_number: current_part,
+			#       upload_id:   mpu_create_response.upload_id,
+			#     })
+      #   }
+      #   part_number = part_number + 1
+      #   part_offset = part_offset + s3_limit
+      # else
+      # ... do put_object
+      # end
+      if file.size > 104857600 #100MB
+        client.create_multipart_upload(bucket_opts)
+      else
+        client.put_object(bucket_opts.merge(body: file))
+      end
     end
 
     private
--- spec/backupsss/backup_spec.rb
+++ spec/backupsss/backup_spec.rb
@@ -11,19 +11,38 @@ describe Backupsss::Backup do
       filename: filename
     }
   end
-  let(:backup) { Backupsss::Backup.new(config_hash, client) }
-  let(:file)   { instance_double('File') }
+  let(:backup)  { Backupsss::Backup.new(config_hash, client) }
+  let(:file)    { instance_double('File') }
   let(:client) { double }
 
   describe '#put_file' do
-    it 'uploads the file to the s3 location defined by the config' do
-      allow(client).to receive(:put_object)
-        .with(bucket: 's3://some_bucket', key: key, body: file)
+    let(:sm) { 1024*1024*100 }
+    let(:md) { 1024*1024*150 }
+    let(:lg) { 1024*1024*200 }
+    context 'when the file size is less than 100 MB' do
+      let(:file) { OpenStruct.new(:size => sm) }
+      it 'uploads the file to the s3 location defined by the config' do
+        allow(client).to receive(:put_object)
+          .with(bucket: 's3://some_bucket', key: key, body: file)
 
-      backup.put_file(file)
+        backup.put_file(file)
 
-      expect(client).to have_received(:put_object)
-        .with(bucket: 's3://some_bucket', key: key, body: file)
+        expect(client).to have_received(:put_object)
+          .with(bucket: 's3://some_bucket', key: key, body: file)
+      end
+    end
+
+    context 'when the file size is greater than 100 MB' do
+      let(:file) { OpenStruct.new(:size => md) }
+      it 'creates a multipart upload' do
+        allow(client).to receive(:create_multipart_upload)
+          .with(bucket: 's3://some_bucket', key: key)
+
+        backup.put_file(file)
+
+        expect(client).to have_received(:create_multipart_upload)
+          .with(bucket: 's3://some_bucket', key: key)
+      end
     end
   end
 end
